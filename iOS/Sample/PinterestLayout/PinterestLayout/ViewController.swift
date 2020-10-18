c80f3caae6abdda85ebc221cd6f6c4a4b0a28be1
yeojaeng <duwjdtn1@gmail.com>
2020-10-18 19:22:45 +0900

    Update: AutoLayout & FlowLayout

--- iOS/Sample/PinterestLayout/PinterestLayout/ViewController.swift
+++ iOS/Sample/PinterestLayout/PinterestLayout/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK:- Outlet
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        collectionView.dataSource = self
     }
-
-
+    
 }
 
+//MARK:- DataSource Methods
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        
+        return cell
+    }
+}
